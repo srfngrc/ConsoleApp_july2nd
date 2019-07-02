1ff6e06e4547131735dabf2896aa7ee27b31f15d
sg <srfngrc@outlook.com>
2019-07-02 19:30:49 -0400

    1st upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_july2nd
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int integerNumber;
+            int i;
+            Console.WriteLine("Please enter an integer N:");
+            int.TryParse(Console.ReadLine(), out integerNumber);
+
+            for (i=1; i <= 10; i++)
+            {
+                Console.WriteLine(integerNumber + " x " + i + "=" + integerNumber * i);
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
