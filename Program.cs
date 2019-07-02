66efae48e791bcbc54f539312427a37fe9b18d27
sg <srfngrc@outlook.com>
2019-07-02 19:56:56 -0400

    2nd upload july2nd 8pm

--- Program.cs
+++ Program.cs
@@ -10,19 +10,41 @@ namespace ConsoleApp_july2nd
 
     class Program
     {
+
         static void Main(string[] args)
+        {
+            CalculateSRFN();
+            
+            Console.WriteLine("Do you wanna calculate it for another integer? (Y/N)");
+            string Answer = Console.ReadLine();
+            if (Answer == "Y")
+            {
+                CalculateSRFN();
+            }
+            else if (Answer == "N")
+            {
+                System.Environment.Exit(1);
+                //Application.Exit();
+            }
+            else
+            {
+                Console.WriteLine("Wrong character. Please enter just 'Y' or 'N'.");
+            }
+            Console.ReadLine();
+        }
+        static private void CalculateSRFN()
         {
             int integerNumber;
             int i;
             Console.WriteLine("Please enter an integer N:");
             int.TryParse(Console.ReadLine(), out integerNumber);
 
-            for (i=1; i <= 10; i++)
+            for (i = 1; i <= 10; i++)
             {
                 Console.WriteLine(integerNumber + " x " + i + "=" + integerNumber * i);
                 Console.WriteLine();
             }
-            Console.ReadLine();
         }
+
     }
 }
